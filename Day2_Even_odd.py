3399f22f6a4772781e94dc77ff65913528729d9c
Phanindra <kmrpolina@gmail.com>
2026-02-10 21:42:52 -0600

    new code using the functions methods

--- Day2_Even_odd.py
+++ Day2_Even_odd.py
@@ -1,9 +1,34 @@
-user_num=float(input("Enter the Number: "))
-if user_num.is_integer():
-    num=int(user_num)
-    if num % 2 ==0:
-        print("Entered Number is Even")
+def get_integer(prompt="Enter a number: "):
+    """ Get vaild input from the user  with custom prompt"""
+    while True:
+        try:
+            num=int(input(prompt))
+            if 0<= num <=100:
+                return num
+            else:
+                print("Enter the valid integer from 0 to 100")
+        except ValueError:
+            print("Please enter valid integer")
+
+
+def is_even(number):
+    ''' return True if the number is even else false for odd number'''
+    return number % 2 == 0
+
+def main():
+    ''' running the even odd program '''
+    num=get_integer("Enter the number (0 to 100): ")
+    result=is_even(num)
+    if result:
+        print(f"{num} is Even")
     else:
-        print("Entered Number is Odd")
-else:
-    print("Entered Number is not Integer, It should be only positive Integer Number")
+        print(f"{num} is Odd")
+
+if __name__=="__main__":
+    main()
+
+    
+
+    
+
+
